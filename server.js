f23a1727333efa2488a550519f7cd4900900adf9
vilasks <vilaskathera4@gmail.com>
2023-02-24 17:29:24 +0530

    moved port to server.js

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const cors = require("cors")
 const app = express()
 require("dotenv").config()
 app.use(cors())
-
+let port = 3000
 app.get("/",(req,res)=>{
     res.send("Hello There")
 })
 
-app.listen(process.env.PORT,(err)=>{
+app.listen(port,(err)=>{
     if(err){
         console.log(err)
     }
-    console.log("started listening on port " + process.env.PORT)
+    console.log("started listening on port " + port)
 })
